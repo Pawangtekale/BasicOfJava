71f30211c14a7f150d5e9542ca2b0c97ab997eab
Pawangtekale <121091609+Pawangtekale@users.noreply.github.com>
2025-06-06 18:42:03 +0530

    Add files via upload

---
+++ Bottal.java
@@ -0,0 +1,17 @@
+class Bottal 
+{
+	String brand="Cello";
+	double price;
+	double capacity;
+	String material;
+	public void displayBottal(){
+		String brand="Local Market";
+		System.out.println("Brand Of Bottal: "+brand);
+		System.out.println("Brand Of Bottal: "+this.brand);
+		System.out.println("Price Of Bottal:"+ price);
+		System.out.println("Capacity Of Bottal:"+ capacity);
+		System.out.println("Material Of Bottal:"+material);
+		System.out.println(this);
+	}
+	
+}
---
+++ BottalDrive.java
@@ -0,0 +1,12 @@
+class BottalDrive 
+{
+	public static void main(String[] args) 
+	{
+		Bottal b1=new Bottal();
+		b1.price=85;
+		b1.material="plastic";
+		b1.capacity=1.5;
+		
+		b1.displayBottal();
+	}
+}
