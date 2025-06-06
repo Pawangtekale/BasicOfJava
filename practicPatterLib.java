84f3f228005c48beff46a8a59b1286bbd2621e99
Pawangtekale <121091609+Pawangtekale@users.noreply.github.com>
2025-06-06 19:35:45 +0530

    Add files via upload

---
+++ practicPatterLib.java
@@ -0,0 +1,35 @@
+class practicPatterLib 
+{
+	public static void main(String[] args) 
+	{
+		int n=12;
+		for (int i=0;i<n;i++)
+		{
+			for (int j=0;j<n;j++)
+			{
+				if (i+j==n-1|| i==j)
+				{
+					System.out.print("*"+" ");
+				}
+				else 
+					System.out.print(" "+" ");
+			}
+			System.out.println();
+		}
+	}
+		
+}
+/* Output of This code is 
+*                     *
+  *                 *
+    *             *
+      *         *
+        *     *
+          * *
+          * *
+        *     *
+      *         *
+    *             *
+  *                 *
+*                     *
+*/
